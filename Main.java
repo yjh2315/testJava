249ed8e10259a48b82d737d140cafae1cedba525
yjh2315 <119277442+yjh2315@users.noreply.github.com>
2022-11-30 06:14:33 +0000

    15596 commit

--- Main.java
+++ Main.java
@@ -6,42 +6,31 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
+
 public class Main {
+    public static long sum(int[] a){
+        long sum=0;
+        for(int i : a){
+            sum = sum+i;
+        }
+        return sum;
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        String tempIn = br.readLine();
-        int re = Integer.parseInt(tempIn); //반복 횟수 입력단
-        for(int j=0;j<re;j++){
-            tempIn = br.readLine();
-            String[] data = tempIn.split(" "); // 점수 입력단
-    
-            int[] nData = new int[data.length-1];
-            float aver = 0;                         //평균
-            int overA = 0;
-            int sum = 0;                             //점수합
-    
-            for(int i=1;i<(data.length);i++){
-                nData[i-1] = Integer.parseInt(data[i]);
-                sum = sum+nData[i-1];
-            }
-            int totalN = nData.length;
-
-            aver = (float)sum/totalN;
-
-            for(int i=0;i<totalN;i++){
-                if(nData[i]>aver){
-                    overA++;
-                }
-            }
-            bw.write(String.format("%.3f",(float)overA*100/totalN)+"%\n");
-            
-
-
+        String inS = br.readLine();
+        String[] inSa = inS.split(" ");
+        int[] inA = new int[inSa.length];
+        for(int i = 0; i<inSa.length; i++){
+            inA[i] = Integer.parseInt(inSa[i]);
         }
+        
+        long res = sum(inA);
+        bw.write(String.format("%d", res)+"\n");
+
         bw.flush();
         bw.close();
         br.close();
-
     }
 }
