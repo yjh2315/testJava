7ebbfc187b0860611ee4904104bb94c7cbae1897
yjh2315 <119277442+yjh2315@users.noreply.github.com>
2022-12-02 06:43:00 +0000

    1152

--- Main.java
+++ Main.java
@@ -8,27 +8,22 @@ import java.io.OutputStreamWriter;
 
 
 public class Main {
-    public static long sum(int[] a){
-        long sum=0;
-        for(int i : a){
-            sum = sum+i;
-        }
-        return sum;
-    }
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        String inS = br.readLine();
-        String[] inSa = inS.split(" ");
-        int[] inA = new int[inSa.length];
-        for(int i = 0; i<inSa.length; i++){
-            inA[i] = Integer.parseInt(inSa[i]);
+        String in = br.readLine();
+        if(in.length()==1&&in.equals(" ")){
+            bw.write("0"+"\n");
+        }else{
+            String[] inA = in.split(" ");
+
+            if(inA[0]==""&&inA.length!=0){
+                bw.write(inA.length-1+"\n");
+            }else{
+                bw.write(inA.length+"\n");
+            }
         }
         
-        long res = sum(inA);
-        bw.write(String.format("%d", res)+"\n");
-
         bw.flush();
         bw.close();
         br.close();
